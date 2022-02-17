e861f709805df4e6295ac0e229f320f9e33622c1
KateKatze <dr.katekogler@gmail.com>
2022-02-17 10:49:53 +0100

    first push

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { MaininfoComponent } from './maininfo/maininfo.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    MaininfoComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
+})
+export class ContactComponent implements OnInit {
+  contact = new FormGroup({
+    firstName: new FormControl("",Validators.required),
+    lastName: new FormControl("", [Validators.required, Validators.minLength(2)]),
+    eMail: new FormControl("", Validators.required),
+    textArea: new FormControl("", Validators.required),
+    newsletter: new FormControl("")
+  })
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+submitForm() {
+  if (this.contact.valid) {
+     var a = this.contact.value;
+  console.log(a);
+  }
+}
+
+}
